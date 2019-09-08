04c90463ffd76a9787e8aa83236d99f3b6dacee8
Fhabiana Machado <thielisantos@hotmail.com>
2019-09-08 12:51:40 -0300

    close: #3 - Nao ha problemas com o calculo de levenshtein

--- src/main/java/br/ufsm/ppgcc/Main.java
+++ src/main/java/br/ufsm/ppgcc/Main.java
@@ -19,119 +19,93 @@ public class Main {
 		int tamanhoMatriz=0;
 		
 		init();
-		
-		/**
-		 * Algoritmo 1 - Separar Campos dos Dados
-		 * @param Documentos pertencentes à mesma coleção
-		 * @return Arquivo com todos os campos: out/saidaAlg1.txt 
-		 * Diretório - resources/json
-		 */
-		Algoritmos.separaCamposDosDados(jsonDir);
-		
-		/**
-		 * Algoritmo 2 - Mesclar estrutura
-		 * @param Arquivo com todos os campos
-		 * @return ArrayList com campos distintos, arquivo lista de referências 1
-		 * Diretório - src/main/resources/artefatos/saidaAlg1.txt
-		 */
-		try {
-			palavras = Util.leArquivo(new File(".").getCanonicalPath()+"/out/etapa1_docEstruturalGeral.txt");
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		palavras = Util.removeRepetidas(palavras);
-//		System.out.println("Palavras "+palavras.size());
-//		for(String s : palavras) {
-//		     System.out.println(s);
-//		}
-//		
-		palavras = Algoritmos.removeRepetidasComListaRef(palavras, "-enddoc");
-//		palavras = Util.removeUmaPalavra(palavras, "--enddoc");
-		tamanhoMatriz = palavras.size();
-		try {
-			Util.gravaArquivoCsv(palavras, new File(".").getCanonicalPath() + "/out/etapa2_campos.csv");
-		} catch (IOException e1) {
-			e1.printStackTrace();
-		}
-		
-		/**
-		 * Algoritmo 3 - Analisar Radical da Palavra
-		 * @param ArrayList com campos distintos
-		 * @return Matriz Stemmer[][]
-		 * Diretório - src/main/resources/artefatos/radical.csv
-		 */
-		int[][] mStem = new int[tamanhoMatriz][tamanhoMatriz];
-		mStem = Algoritmos.geraMatrizStemmer( Algoritmos.aplicaStemmerList(palavras) );
-//		Util.mostraMatriz(mStem);
-		try {
-			Util.gravaMatrizParaCsv(mStem, new File(".").getCanonicalPath()+"/out/etapa2_matrizRadical.csv");
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-		
-		/*
-		 * Algoritmo 4 - Analisar Similaridade Baseada em Caractere
-		 * @param ArrayList com campos distintos
-		 * @return Matriz Lev[][]
-		 * Diretório - src/main/resources/artefatos/levenshtein.csv
-		 */
-		double[][] mLev = new double[tamanhoMatriz][tamanhoMatriz];
-		mLev = Algoritmos.aplicaLevenshtein(palavras);
-//		Util.mostraMatriz(mLev);
-		try {
-			Util.gravaMatrizParaCsv(mLev, new File(".").getCanonicalPath()+"/out/etapa2_matrizLevenshtein.csv");
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-		
-		/*
-		 * Algoritmo 5 - Analisar Similaridade Baseada em Conhecimento
-		 * @param ArrayList com campos distintos
-		 * @return Matriz Lin[][]
-		 * Diretório - src/main/resources/artefatos/lin.csv
-		 */
-		double[][] mLin = new double[tamanhoMatriz][tamanhoMatriz];
-		mLin = Algoritmos.aplicaLin(palavras);
-//		Util.mostraMatriz(mLin);
-		try {
-			Util.gravaMatrizParaCsv(mLev, new File(".").getCanonicalPath()+"/out/etapa2_matrizLin.csv");
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
 
-		/*
-		 * Algoritmo 6 - Calcular Equivalência
-		 * @param 3 matrizes das técnicas e tamanho da matriz
-		 * @return Matriz Resultados[][]
-		 */
-		double[][] resultados = new double[tamanhoMatriz][tamanhoMatriz];
-		resultados = Algoritmos.calculaEquivalencia(mStem, mLev, mLin, tamanhoMatriz, pontoCorte, pontoCorteAHP);
-//		Util.mostraMatriz(resultados);
-		try {
-			Util.gravaMatrizParaCsv(resultados, new File(".").getCanonicalPath()+"/out/etapa2_resultados.csv");
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		try{ 
+			String arqDocEstruturalGeral = new File(".").getCanonicalPath()+"/out/etapa1_docEstruturalGeral.txt";
+			String arqCampos = new File(".").getCanonicalPath()+"/out/etapa2_campos.csv";
+			String arqMatrizResultados = new File(".").getCanonicalPath()+"/out/etapa2_resultados.csv";
+			String arqMatrizRadical = new File(".").getCanonicalPath()+"/out/etapa2_matrizRadical.csv";
+			String arqMatrizLevenshtein = new File(".").getCanonicalPath()+"/out/etapa2_matrizLevenshtein.csv";
+			String arqMatrizLin = new File(".").getCanonicalPath()+"/out/etapa2_matrizLin.csv";
+			String arqLista2 = new File(".").getCanonicalPath()+"/out/etapa3_listaReferencias2.csv";
+			String arqConsolidados = new File(".").getCanonicalPath()+"/out/etapa3_camposConsolidados.csv";
+		
+			/**
+			 * Algoritmo 1 - Separar Campos dos Dados
+			 * @param Documentos pertencentes à mesma coleção
+			 * Diretório - resources/json
+			 */
+			Algoritmos.separaCamposDosDados(jsonDir);
+		
+			/**
+			 * Algoritmo 2 - Mesclar estrutura
+			 * @param Arquivo com todos os arqCampos
+			 * @return ArrayList com arqCampos distintos, arquivo lista de referências 1
+			 */
+			palavras = Util.leArquivo(arqDocEstruturalGeral);
+			palavras = Util.removeRepetidas(palavras);
+			palavras = Algoritmos.removeRepetidasComListaRef(palavras, "-enddoc");
+	//		palavras = Util.removeUmaPalavra(palavras, "--enddoc");
+			tamanhoMatriz = palavras.size();
+			Util.gravaArquivoCsv(palavras, arqCampos);
+		
+			/**
+			 * Algoritmo 3 - Analisar Radical da Palavra
+			 * @param ArrayList com arqCampos distintos
+			 * @return Matriz Stemmer[][]
+			 */
+			int[][] mStem = new int[tamanhoMatriz][tamanhoMatriz];
+			mStem = Algoritmos.geraMatrizStemmer( Algoritmos.aplicaStemmerList(palavras) );
+	//		Util.mostraMatriz(mStem);
+			Util.gravaMatrizParaCsv(mStem, arqMatrizRadical);
+		
+		
+			/*
+			* Algoritmo 4 - Analisar Similaridade Baseada em Caractere
+			* @param ArrayList com arqCampos distintos
+			* @return Matriz Lev[][]
+			*/
+			double[][] mLev = new double[tamanhoMatriz][tamanhoMatriz];
+			mLev = Algoritmos.aplicaLevenshtein(palavras);
+	//		Util.mostraMatriz(mLev);
+			Util.gravaMatrizParaCsv(mLev, arqMatrizLevenshtein);
+		
+		
+			/*
+			* Algoritmo 5 - Analisar Similaridade Baseada em Conhecimento
+			* @param ArrayList com arqCampos distintos
+			* @return Matriz Lin[][]
+			* Diretório - src/main/resources/artefatos/lin.csv
+			*/
+			double[][] mLin = new double[tamanhoMatriz][tamanhoMatriz];
+			mLin = Algoritmos.aplicaLin(palavras);
+	//		Util.mostraMatriz(mLin);
+			Util.gravaMatrizParaCsv(mLev, arqMatrizLin);
 
-		/*
-		 * Algoritmo 7 - Consolidar estrutura
-		 * @param arquivos de entrada da matriz única e dos campos e arquivos de saída
-		 */
-		try {
-			String campos = new File(".").getCanonicalPath()+"/out/etapa2_campos.csv";
-			String matriz = new File(".").getCanonicalPath()+"/out/etapa2_resultados.csv";
-			String lista2 = new File(".").getCanonicalPath()+"/out/etapa3_listaReferencias2.csv";
-			String consolidados = new File(".").getCanonicalPath()+"/out/etapa3_camposConsolidados.csv";
-			Algoritmos.consolidaEstrutura(campos, matriz, lista2, consolidados);
+
+			/*
+			* Algoritmo 6 - Calcular Equivalência
+			* @param 3 matrizes das técnicas e tamanho da matriz
+			* @return Matriz Resultados[][]
+			*/
+			double[][] resultados = new double[tamanhoMatriz][tamanhoMatriz];
+			resultados = Algoritmos.calculaEquivalencia(mStem, mLev, mLin, tamanhoMatriz, pontoCorte, pontoCorteAHP);
+	//		Util.mostraMatriz(resultados);
+			Util.gravaMatrizParaCsv(resultados, arqMatrizResultados);
+
+			/*
+			* Algoritmo 7 - Consolidar estrutura
+			* @param arquivos de entrada da matriz única e dos arqCampos e arquivos de saída
+			*/
+			Algoritmos.consolidaEstrutura(arqCampos, arqMatrizResultados, arqLista2, arqConsolidados);
+		
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 
 		/**
 		 * Algoritmo 8 - Remontar Estrutura
-		 * @param campos consolidados, lis de referencias 1, lista de referências 2
+		 * @param arqCampos arqConsolidados, lis de referencias 1, lista de referências 2
 		 * @return estrutura consolidada
 		 */
 //		try {
@@ -158,7 +132,7 @@ public class Main {
 		);
 		
 		Scanner ler = new Scanner(System.in);
-		System.out.println("Bem vindo ao sistema para definição de campos equivalentes em fontes de dados JSON");
+		System.out.println("Bem vindo ao sistema para definição de arqCampos equivalentes em fontes de dados JSON");
 		System.out.println("O nome dos arquivos devem ser do modelo doc1.json, doc2.json...");
 		
 		try {
