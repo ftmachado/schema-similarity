81ae248223f9c9aa49b5b5263edf4b8c310a174f
Fhabiana Machado <thielisantos@hotmail.com>
2019-09-10 00:44:04 -0300

    Ajustes para testes

--- src/main/java/br/ufsm/ppgcc/algoritmos/Algoritmos.java
+++ src/main/java/br/ufsm/ppgcc/algoritmos/Algoritmos.java
@@ -4,7 +4,6 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
